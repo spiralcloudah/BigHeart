b78ff3c990eab1804b2a5f6d75e1fb068dda591d
Gene Nartey <genenartey@fb.com>
2019-07-19 17:16:42 -0700

    slight trouble with API call will fix up this weekend

--- app/src/main/java/com/codepath/bigheartapp/ComposeActivity.java
+++ app/src/main/java/com/codepath/bigheartapp/ComposeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -20,12 +21,25 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import com.codepath.bigheartapp.model.Post;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+
+import cz.msebera.android.httpclient.Header;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Float.parseFloat;
 
 public class ComposeActivity extends AppCompatActivity {
 
@@ -46,6 +60,12 @@ public class ComposeActivity extends AppCompatActivity {
     public String photoFileName = "photo.jpg";
     public boolean isEvent = false;
     File photoFile;
+
+    private String API_KEY = "AIzaSyBlqBLcO4u2GXQ8utsYRlsV55kmCavovfI";
+    private String BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?";
+
+    public String lat;
+    public String lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,19 +121,97 @@ public class ComposeActivity extends AppCompatActivity {
                 final String day = sDay.getSelectedItem().toString();
                 final String time = sTime.getSelectedItem().toString() + " " + sAmPm.getSelectedItem().toString();
                 final String year = sYear.getSelectedItem().toString();
+                final String location = etLocation.getText().toString().replace(" ","+");
+
 
                 // create HttpRequest for location and fetch geocode for address inputted
+                // new GetCoordinates().execute(etLocation.getText().toString().replace(" ","+"));
 
-                createPost(description, user, month, day, year, time);
+                 createPost(description, user, month, day, year, time, location);
             }
         });
 
     }
 
-    public void createPost(String description, ParseUser user, String month, String day, String year, String time) {
+    private String getCoordinates(String location) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("address", location );
+        params.put("key", API_KEY );
+        client.get(BASE_URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    Log.d("ComposeActivity", "Request Success!");
+                    // retrieve json lat and long coordinates
+                    lat = ((JSONArray)response.get("results")).getJSONObject(0).getJSONObject("geometry")
+                            .getJSONObject("location").get("lat").toString();
+
+
+                    lng = ((JSONArray)response.get("results")).getJSONObject(0).getJSONObject("geometry")
+                            .getJSONObject("location").get("lat").toString();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                try {
+                    Log.d("ComposeActivity", "Request Success!");
+                    // retrieve json lat and long coordinates
+                    lat = response.getJSONObject(0).getJSONObject("geometry")
+                            .getJSONObject("location").get("lat").toString();
+
+
+                    lng = response.getJSONObject(0).getJSONObject("geometry")
+                            .getJSONObject("location").get("lng").toString();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("ComposeActivity", "Request Failure.");
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.d("ComposeActivity", "Request Success!");
+                throwable.printStackTrace();
+            }
+
+
+        });
+
+        return (lat + "," + lng);
+    }
+
+    public void createPost(String description, ParseUser user, String month, String day, String year, String time, String location) {
         final Post newPost = new Post();
         newPost.setDescription(description);
         newPost.setUser(user);
+
+        // input location geocode .. but first convert strings to floats
+//        Float latitude = parseFloat(lat);
+//        Float longitude = parseFloat(lng);
+        String coordinates = getCoordinates(location);
+        String[] latlng = coordinates.split(",");
+         Double[] fixed = new Double[2];
+
+        for (int i = 0; i < latlng.length; i++) {
+            fixed[i] = parseDouble(latlng[i]);
+        }
+
+        ParseGeoPoint point = new ParseGeoPoint(fixed[0],fixed[1]);
+        newPost.setLocation(point);
         newPost.setIsEvent(isEvent);
 
         if(isEvent) {
@@ -195,4 +293,5 @@ public class ComposeActivity extends AppCompatActivity {
             }
         }
     }
+
 }
