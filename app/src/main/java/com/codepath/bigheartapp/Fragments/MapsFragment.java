476608bdd9fef053293eb032713837c7cf2b433f
Elana Hummel <ehummel@fb.com>
2019-07-22 16:29:16 -0700

    create mapview on maps fragment

--- app/src/main/java/com/codepath/bigheartapp/Fragments/MapsFragment.java
+++ app/src/main/java/com/codepath/bigheartapp/Fragments/MapsFragment.java
@@ -1,18 +1,55 @@
 package com.codepath.bigheartapp.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codepath.bigheartapp.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapsFragment extends Fragment {
+import java.util.Map;
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mapView;
+    View view;
+
+    //required empty constructor
+    public MapsFragment() {}
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_maps, parent, false);
+        view = inflater.inflate(R.layout.fragment_maps, parent, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        if(mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
 
