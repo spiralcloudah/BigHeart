1742fd30f2e8450b8dd7322d33888cd85cca5b3c
Ariana Yu-Chia Harvey <ayharvey@fb.com>
2019-07-24 13:06:15 -0700

    event fragment

--- app/src/main/java/com/codepath/bigheartapp/Fragments/EventFragment.java
+++ app/src/main/java/com/codepath/bigheartapp/Fragments/EventFragment.java
@@ -2,16 +2,125 @@ package com.codepath.bigheartapp.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.codepath.bigheartapp.EndlessRecyclerViewScrollListener;
+import com.codepath.bigheartapp.PostAdapter;
 import com.codepath.bigheartapp.R;
+import com.codepath.bigheartapp.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.parse.Parse.getApplicationContext;
 
 public class EventFragment extends Fragment {
+    // Store a member variable for the listener
+    private EndlessRecyclerViewScrollListener scrollListener;
+
+    ArrayList<Post> posts;
+    public RecyclerView rvEventPosts;
+    PostAdapter adapter;
+    private SwipeRefreshLayout swipeContainer;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_event, parent, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_event, container, false);
+
+        posts = new ArrayList<>();
+
+        adapter = new PostAdapter(posts, 0);
+        rvEventPosts = (RecyclerView) rootView.findViewById(R.id.rvEventPosts);
+
+        rvEventPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvEventPosts.setAdapter(adapter);
+
+        // Configure the RecyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvEventPosts.setLayoutManager(linearLayoutManager);
+
+        // Retain an instance so that you can call `resetState()` for fresh searches
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+
+                view.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //not quite sure if we need this?
+                    }
+                });
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        rvEventPosts.addOnScrollListener(scrollListener);
+
+
+
+        swipeContainer = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code here
+                Toast.makeText(getApplicationContext(), "Refreshed!", Toast.LENGTH_LONG).show();
+                // To keep animation for 4 seconds
+                posts.clear();
+                adapter.clear();
+                loadTopPosts();
+            }
+        });
+
+        // Scheme colors for animation
+        swipeContainer.setColorSchemeColors(
+                getResources().getColor(android.R.color.holo_blue_bright),
+                getResources().getColor(android.R.color.holo_green_light),
+                getResources().getColor(android.R.color.holo_orange_light),
+                getResources().getColor(android.R.color.holo_red_light)
+        );
+
+
+
+        loadTopPosts();
+
+
+        return rootView;
+
+    }
+
+    public void loadTopPosts(){
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+        postQuery.addDescendingOrder(Post.KEY_DATE);
+
+        postQuery.whereEqualTo(Post.KEY_IS_EVENT, true);
+
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if(e == null) {
+                    adapter.clear(getView());
+                    for(int i = 0; i < objects.size(); i++) {
+                        posts.add(objects.get(i));
+                        adapter.notifyItemInserted(posts.size() - 1);
+                    }
+                } else {
+                    Toast.makeText(getContext(), "Failed to query posts", Toast.LENGTH_SHORT).show();
+                }
+                swipeContainer.setRefreshing(false);
+            }
+        });
     }
 }
