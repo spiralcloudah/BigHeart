3eee6645a99a0689d7db351c2ae61c85fff3f597
Ariana Yu-Chia Harvey <ayharvey@fb.com>
2019-07-30 11:41:36 -0700

    cleaned up code in Post.class

--- app/src/main/java/com/codepath/bigheartapp/model/Post.java
+++ app/src/main/java/com/codepath/bigheartapp/model/Post.java
@@ -15,113 +15,94 @@ import java.util.ArrayList;
 
 @ParseClassName("Post")
 public class Post extends ParseObject implements Serializable {
+
+    // Set unchangeable variables to specified columns in the parse database
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_LOCATION = "location";
     public static final String KEY_USER = "userId";
     public static final String KEY_DATE = "createdAt";
     public static final String KEY_IS_EVENT = "isEvent";
-
     public static final String KEY_DAY = "day";
-
     public static final String KEY_TIME = "time";
     public static final String KEY_LIKED_BY = "hearts";
     public static final String KEY_EVENT_TITLE = "eventTitle";
     public static final String KEY_ADDRESS = "address";
     public static final String KEY_BOOKMARKED = "interested";
 
-    public void setEventTitle(String title) {
-        put(KEY_EVENT_TITLE, title);
-    }
-
-    public String getEventTitle() {
-        return getString(KEY_EVENT_TITLE);
-    }
-
+    // Configure description
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
     }
-
     public void setDescription(String description) {
         put(KEY_DESCRIPTION, description);
     }
 
-    public boolean getIsEvent() {
-        return getBoolean(KEY_IS_EVENT);
-    }
-
-    public void setIsEvent(boolean isEvent) {
-        put(KEY_IS_EVENT, isEvent);
-    }
-
-    public ParseGeoPoint getLocation() {
-        return getParseGeoPoint(KEY_LOCATION);
-    }
-
-    public void setLocation(ParseGeoPoint location) {
-        put(KEY_LOCATION, location);
-    }
-
+    // Configure image
     public ParseFile getImage() {
         return getParseFile(KEY_IMAGE);
     }
-
     public void setImage(ParseFile image){
         put(KEY_IMAGE, image);
     }
 
+    // Configure location
+    public ParseGeoPoint getLocation() {
+        return getParseGeoPoint(KEY_LOCATION);
+    }
+    public void setLocation(ParseGeoPoint location) {
+        put(KEY_LOCATION, location);
+    }
+
+    // Configure user
     public ParseUser getUser() {
         return getParseUser(KEY_USER);
     }
-
     public void setUser(ParseUser user){
         put(KEY_USER, user);
     }
 
-
-
-
-    public String getAddress() {
-        return getString(KEY_ADDRESS);
+    // Configure post/event status
+    public boolean getIsEvent() {
+        return getBoolean(KEY_IS_EVENT);
+    }
+    public void setIsEvent(boolean isEvent) {
+        put(KEY_IS_EVENT, isEvent);
     }
 
-    public void setAddress(String address) {
-        put(KEY_ADDRESS, address);
-    }
-
+    // Configure day
     public String getDay() {
         return getString(KEY_DAY);
     }
-
     public void setDay(String day) {
         put(KEY_DAY, day);
     }
 
+    // Configure time
     public String getTime() {
         return getString(KEY_TIME);
     }
-
     public void setTime(String time) {
         put(KEY_TIME, time);
     }
 
-    //Likes
+    // Configure likes/hearts
     public JSONArray getLikes() {
         return getJSONArray(KEY_LIKED_BY);
     }
-
-    public int getNumLikes() { return getLikes().length(); }
-
     public void likePost(ParseUser user) {
         add(KEY_LIKED_BY, user);
     }
-
     public void unlikePost(ParseUser user) {
         ArrayList<ParseUser> a = new ArrayList<>();
         a.add(user);
         removeAll(KEY_LIKED_BY, a);
     }
 
+    // Get the number of likes
+    public int getNumLikes() { return getLikes().length(); }
+
+    // Check to see whether a post is liked
     public boolean isLiked() {
         JSONArray a = getLikes();
         if(a != null) {
@@ -138,23 +119,39 @@ public class Post extends ParseObject implements Serializable {
         return false;
     }
 
-    //Bookmarks
+    // Configure event title
+    public void setEventTitle(String title) {
+        put(KEY_EVENT_TITLE, title);
+    }
+    public String getEventTitle() {
+        return getString(KEY_EVENT_TITLE);
+    }
+
+    // Configure address
+    public String getAddress() {
+        return getString(KEY_ADDRESS);
+    }
+    public void setAddress(String address) {
+        put(KEY_ADDRESS, address);
+    }
+
+    // Configure bookmarks
     public JSONArray getBookmarked() {
         return getJSONArray(KEY_BOOKMARKED);
     }
-
-    public int getNumBookmarks() { return getBookmarked().length(); }
-
     public void bookmarkPost(ParseUser user) {
         add(KEY_BOOKMARKED, user);
     }
-
     public void unbookmarkPost(ParseUser user) {
         ArrayList<ParseUser> a = new ArrayList<>();
         a.add(user);
         removeAll(KEY_BOOKMARKED, a);
     }
 
+    // Get the number of bookmarked events
+    public int getNumBookmarks() { return getBookmarked().length(); }
+
+    // Check to see whether an event is bookmarked
     public boolean isBookmarked() {
         JSONArray a = getBookmarked();
         if(a != null) {
@@ -171,18 +168,15 @@ public class Post extends ParseObject implements Serializable {
         return false;
     }
 
-    //Querying
+    // Querying for posts
     public static class Query extends ParseQuery<Post> {
-
         public Query() {
             super(Post.class);
         }
-
         public Query getTop() {
             setLimit(20);
             return this;
         }
-
         public Query withUser() {
             include("user");
             return this;
