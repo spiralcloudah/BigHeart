3fc57561c1ce0740bc9f7dd8154e586a9ab94a06
Elana Hummel <ehummel@fb.com>
2019-07-31 10:28:28 -0700

    make a base fragment interface for querying posts and other shared attributes

--- app/src/main/java/com/codepath/bigheartapp/Fragments/HomeFragment.java
+++ app/src/main/java/com/codepath/bigheartapp/Fragments/HomeFragment.java
@@ -1,10 +1,6 @@
 package com.codepath.bigheartapp.Fragments;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -15,28 +11,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.codepath.bigheartapp.ComposeActivity;
 import com.codepath.bigheartapp.EndlessRecyclerViewScrollListener;
-import com.codepath.bigheartapp.PostAdapter;
 import com.codepath.bigheartapp.PostDetailsActivity;
 import com.codepath.bigheartapp.R;
+import com.codepath.bigheartapp.helpers.FragmentHelper;
 import com.codepath.bigheartapp.model.Post;
-import com.parse.FindCallback;
-import com.parse.ParseException;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static android.app.Activity.RESULT_CANCELED;
-import static android.app.Activity.RESULT_OK;
-
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements FragmentHelper.BaseFragment {
 
     // Store variables to use in the home fragment
     private EndlessRecyclerViewScrollListener scrollListener;
-    public static ArrayList<Post> posts;
     public static RecyclerView rvPost;
-    public static PostAdapter adapter;
     private SwipeRefreshLayout swipeContainer;
 
     @Override
@@ -48,14 +35,10 @@ public class HomeFragment extends Fragment {
         getActivity().registerReceiver(detailsChangedReceiver, filter);
 
 
-
-
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Set created variables to new elements or corresponding layouts
-        posts = new ArrayList<>();
-        adapter = new PostAdapter(posts, 0);
+
         rvPost = (RecyclerView) rootView.findViewById(R.id.rvPost);
         rvPost.setLayoutManager(new LinearLayoutManager(getContext()));
         rvPost.setAdapter(adapter);
@@ -78,6 +61,7 @@ public class HomeFragment extends Fragment {
         // Adds the scroll listener and item decoration to RecyclerView
         rvPost.addOnScrollListener(scrollListener);
         rvPost.addItemDecoration(new VerticalSpaceItemDecoration(12));
+        rvPost.addItemDecoration(new HorizontalSpaceItemDecoration(6));
         swipeContainer = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeContainer);
 
         // Setup refresh listener which triggers new data loading
@@ -99,52 +83,38 @@ public class HomeFragment extends Fragment {
                 getResources().getColor(android.R.color.holo_orange_light),
                 getResources().getColor(android.R.color.holo_red_light)
         );
+
+
         loadTopPosts();
         return rootView;
     }
 
     // load the latest posts
     public void loadTopPosts(){
-        final Post.Query postQuery = new Post.Query();
-        postQuery.getTop().withUser();
-        postQuery.addDescendingOrder(Post.KEY_DATE);
-        postQuery.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> objects, ParseException e) {
-                if(e == null) {
-                    adapter.clear();
-                    for(int i = 0; i < objects.size(); i++) {
-                        posts.add(objects.get(i));
-                        adapter.notifyItemInserted(posts.size() - 1);
-                    }
-                } else {
-                    Toast.makeText(getContext(), "Failed to query posts", Toast.LENGTH_LONG).show();
-                }
-                swipeContainer.setRefreshing(false);
-            }
-        });
+        adapter.clear();
+        FragmentHelper fragmentHelper = new FragmentHelper(getPostQuery());
+        fragmentHelper.fetchPosts(this);
+        swipeContainer.setRefreshing(false);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+    public Post.Query getPostQuery() {
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+        postQuery.addDescendingOrder(Post.KEY_DATE);
+        return postQuery;
     }
 
-    //put space between cardviews
-    public class VerticalSpaceItemDecoration extends RecyclerView.ItemDecoration {
+    @Override
+    public void onFetchSuccess(List<Post> objects, int i) {
+        posts.add(objects.get(i));
+        adapter.notifyItemInserted(posts.size() - 1);
+    }
 
-        // Specify a final variable for space between cardviews
-        private final int verticalSpaceHeight;
-
-        // function to set the space height
-        public VerticalSpaceItemDecoration(int verticalSpaceHeight) {
-            this.verticalSpaceHeight = verticalSpaceHeight;
-        }
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
-                                   RecyclerView.State state) {
-            outRect.top = verticalSpaceHeight;
-        }
+    @Override
+    public void onFetchFailure() {
+        Toast.makeText(getContext(), "Failed to query posts", Toast.LENGTH_LONG).show();
+        swipeContainer.setRefreshing(false);
     }
 
 
@@ -155,30 +125,4 @@ public class HomeFragment extends Fragment {
         // Unregister the listener when the application is paused
         getActivity().unregisterReceiver(detailsChangedReceiver);
     }
-
-    // Define the callback for what to do when data is received
-    private BroadcastReceiver detailsChangedReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-
-            int resultCode = intent.getIntExtra(getString(R.string.result_code), RESULT_CANCELED);
-
-            if (resultCode == RESULT_OK) {
-                Post postChanged = (Post) intent.getSerializableExtra(Post.class.getSimpleName());
-                int indexOfChange = -1;
-                for (int i = 0; i < posts.size(); i++) {
-                    if (posts.get(i).hasSameId(postChanged)) {
-                        indexOfChange = i;
-                        break;
-                    }
-                }
-                if (indexOfChange != -1) {
-                    posts.set(indexOfChange, postChanged);
-                    adapter.notifyItemChanged(indexOfChange);
-                } else {
-                    Toast.makeText(getContext(), "An error occurred", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        }
-    };
 }
