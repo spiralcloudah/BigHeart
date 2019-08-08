17a3a6aef78c90eee8259d0aa82e244e0d09aa60
Ariana Yu-Chia Harvey <ayharvey@fb.com>
2019-08-08 11:22:42 -0700

    color marker change in maps fragment

--- app/src/main/java/com/codepath/bigheartapp/Fragments/MapsFragment.java
+++ app/src/main/java/com/codepath/bigheartapp/Fragments/MapsFragment.java
@@ -190,10 +190,10 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback, FetchR
                 BitmapDescriptor coloredIcon;
                 if (objects.get(i).getIsEvent()) {
                     // Events have a blue icon
-                    coloredIcon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE);
+                    coloredIcon = BitmapDescriptorFactory.defaultMarker(305);
 
                 } else {
-                    coloredIcon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN);
+                    coloredIcon = BitmapDescriptorFactory.defaultMarker(190);
                 }
                 mGoogleMap.addMarker(new MarkerOptions()
                         .position(pos)
