fada37e4a83964b565625bf199fe55a279b73c46
Elana Hummel <ehummel@fb.com>
2019-08-08 15:25:03 -0700

    fix toast in compose

--- app/src/main/java/com/codepath/bigheartapp/ComposeActivity.java
+++ app/src/main/java/com/codepath/bigheartapp/ComposeActivity.java
@@ -381,7 +381,6 @@ public class ComposeActivity extends AppCompatActivity {
 
                 // Result was a failure
             } else {
-                Toast.makeText(ComposeActivity.this, "No picture taken!", Toast.LENGTH_LONG).show();
                 photoFile = null;
             }
         }
